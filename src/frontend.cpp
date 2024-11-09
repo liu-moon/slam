8d5cd3e308dfccdc6b4467f58c9c9c5efc986eac
liuiu <liu.moon.710@gmail.com>
2024-11-09 13:10:33 +0800

    add book_code

---
+++ src/frontend.cpp
@@ -0,0 +1,87 @@
+bool Frontend::AddFrame(myslam::Frame::Ptr frame) {
+    current_frame_ = frame;
+
+    switch (status_) {
+        case FrontendStatus::INITING:
+            StereoInit();
+            break;
+        case FrontendStatus::TRACKING_GOOD:
+        case FrontendStatus::TRACKING_BAD:
+            Track();
+            break;
+        case FrontendStatus::LOST:
+            Reset();
+            break;
+    }
+
+    last_frame_ = current_frame_;
+    return true;
+}
+
+bool Frontend::Track() {
+    if (last_frame_) {
+        current_frame_->SetPose(relative_motion_ * last_frame_->Pose());
+    }
+
+    int num_track_last = TrackLastFrame();
+    tracking_inliers_ = EstimateCurrentPose();
+
+    if (tracking_inliers_ > num_features_tracking_) {
+        // tracking good
+        status_ = FrontendStatus::TRACKING_GOOD;
+    } else if (tracking_inliers_ > num_features_tracking_bad_) {
+        // tracking bad
+        status_ = FrontendStatus::TRACKING_BAD;
+    } else {
+        // lost
+        status_ = FrontendStatus::LOST;
+    }
+
+    InsertKeyframe();
+    relative_motion_ = current_frame_->Pose() * last_frame_->Pose().inverse();
+
+    if (viewer_) viewer_->AddCurrentFrame(current_frame_);
+    return true;
+}
+
+int Frontend::TrackLastFrame() {
+    // use LK flow to estimate points in the right image
+    std::vector<cv::Point2f> kps_last, kps_current;
+    for (auto &kp : last_frame_->features_left_) {
+        if (kp->map_point_.lock()) {
+            // use project point
+            auto mp = kp->map_point_.lock();
+            auto px =
+                camera_left_->world2pixel(mp->pos_, current_frame_->Pose());
+            kps_last.push_back(kp->position_.pt);
+            kps_current.push_back(cv::Point2f(px[0], px[1]));
+        } else {
+            kps_last.push_back(kp->position_.pt);
+            kps_current.push_back(kp->position_.pt);
+        }
+    }
+
+    std::vector<uchar> status;
+    Mat error;
+    cv::calcOpticalFlowPyrLK(
+        last_frame_->left_img_, current_frame_->left_img_, kps_last,
+        kps_current, status, error, cv::Size(11, 11), 3,
+        cv::TermCriteria(cv::TermCriteria::COUNT + cv::TermCriteria::EPS, 30,
+                         0.01),
+        cv::OPTFLOW_USE_INITIAL_FLOW);
+
+    int num_good_pts = 0;
+
+    for (size_t i = 0; i < status.size(); ++i) {
+        if (status[i]) {
+            cv::KeyPoint kp(kps_current[i], 7);
+            Feature::Ptr feature(new Feature(current_frame_, kp));
+            feature->map_point_ = last_frame_->features_left_[i]->map_point_;
+            current_frame_->features_left_.push_back(feature);
+            num_good_pts++;
+        }
+    }
+
+    LOG(INFO) << "Find " << num_good_pts << " in the last image.";
+    return num_good_pts;
+}
